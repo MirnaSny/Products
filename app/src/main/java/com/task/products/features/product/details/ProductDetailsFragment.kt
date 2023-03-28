07cc005a2a0e8546928fb5139bc62bb513929a32
MirnaSny <116234495+MirnaSny@users.noreply.github.com>
2023-03-28 12:17:57 +0300

    update it

--- app/src/main/java/com/task/products/features/product/details/ProductDetailsFragment.kt
+++ app/src/main/java/com/task/products/features/product/details/ProductDetailsFragment.kt
@@ -2,12 +2,24 @@ package com.task.products.features.product.details
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
+import com.task.products.databinding.ActivityMainBinding
+import com.task.products.databinding.ActivityProductDetailsBinding
+import com.task.products.features.main.MainViewModel
+import com.task.products.features.main.adapter.ViewPagerAdapter
 
-class ProductDetailsFragment : Fragment() {
+class ProductDetailsFragment : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityProductDetailsBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityProductDetailsBinding.inflate(layoutInflater)
+
+
 
 
 
